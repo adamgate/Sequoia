3b4367ad90d7198721599050d7cfaeddcb8c6e36
adamgate <applegateadaml@gmail.com>
2025-12-10 11:17:50 -0700

    add note service

---
+++ Sequoia.Web.Client/Services/Contracts/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using Sequoia.Core.Models;
+
+namespace Sequoia.Web.Client.Services.Contracts
+{
+    public interface INoteService
+    {
+        Task<IEnumerable<Note>> GetNotesByUserIdAsync(int userId);
+        Task<Note> GetNoteByIdAsync(int noteId);
+        Task CreateNoteAsync(Note note);
+        Task UpdateNoteAsync(Note note);
+        Task DeleteNoteByIdAsync(int noteId);
+    }
+}
---
+++ Sequoia.Web.Client/Services/Implementations/NoteService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+
+using Sequoia.Core.Models;
+using Sequoia.Web.Client.Services.Contracts;
+
+namespace Sequoia.Web.Client.Services.Implementations
+{
+    public class NoteService : INoteService
+    {
+        private HttpClient _httpClient;
+
+        public NoteService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Note>> GetNotesByUserIdAsync(int userId)
+        {
+            var route = $"api/user/{userId}/notes";
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Note>>(route);
+        }
+    }
+}
